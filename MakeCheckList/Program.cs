b4f06977fb16dced0d4741b4771c498478faf936
MadBuffoon <turboboyruss@gmail.com>
2023-10-24 19:33:47 -0500

    Add more check with the MakeCheckList.csproj

--- MakeCheckList/Program.cs
+++ MakeCheckList/Program.cs
@@ -18,64 +18,76 @@ internal class Program
         string outputFile = Path.Combine(exeDirectory, outputFileName);
         string linksFile = Path.Combine(exeDirectory, linksFileName);
 
-        if (Directory.Exists(directoryPath) && File.Exists(linksFile))
+        try
         {
-            string[] lines = File.ReadAllLines(linksFile); // Read all lines from FileLinks.txt
-
-            string[] links = new string[lines.Length]; // Array to store links
-            string[] names = new string[lines.Length]; // Array to store names
-
-            for (int i = 0; i < lines.Length; i++)
+            if (Directory.Exists(directoryPath))
             {
-                string[] parts = lines[i].Split(new char[] { ' ' }, 2, StringSplitOptions.RemoveEmptyEntries);
-        
-                if (parts.Length >= 2)
+                string[] lines = File.Exists(linksFile) ? File.ReadAllLines(linksFile) : new string[0]; // Read all lines from FileLinks.txt or create an empty array
+
+                string[] links = new string[lines.Length]; // Array to store links
+                string[] names = new string[lines.Length]; // Array to store names
+
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    names[i] = parts[0]; // Store the name
-                    links[i] = parts[1]; // Store the link
-                }
-                else if (parts.Length == 1)
-                {
-                    names[i] = ""; // No name found
-                    links[i] = parts[0]; // Store the link
-                }
-            }
+                    string[] parts = lines[i].Split(new char[] { ' ' }, 2, StringSplitOptions.RemoveEmptyEntries);
 
-            using (StreamWriter writer = new StreamWriter(outputFile))
-            {
-                // Recursively get all file paths in the directory and its subdirectories
-                string[] filePaths = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
-                int totalFiles = filePaths.Length;
-
-                for (int i = 0; i < totalFiles; i++)
-                {
-                    string filePath = filePaths[i];
-                    string relativePath = filePath.Substring(directoryPath.Length + 1);
-                    string fileChecksum = CalculateFileChecksum(filePath, i + 1, totalFiles);
-
-                    // Append the link to the line in CheckList.txt
-                    string line = $"{relativePath} {fileChecksum}";
-
-                    if (i < links.Length)
+                    if (parts.Length >= 2)
                     {
-                        line += " " + links[i]; // Append the link
+                        names[i] = parts[0]; // Store the name
+                        links[i] = parts[1]; // Store the link
+                    }
+                    else if (parts.Length == 1)
+                    {
+                        names[i] = ""; // No name found
+                        links[i] = parts[0]; // Store the link
+                    }
+                }
+
+                if (!File.Exists(linksFile))
+                {
+                    links = new string[] { "null" };
+                }
+
+                using (StreamWriter writer = new StreamWriter(outputFile))
+                {
+                    // Recursively get all file paths in the directory and its subdirectories
+                    string[] filePaths = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+                    int totalFiles = filePaths.Length;
+
+                    for (int i = 0; i < totalFiles; i++)
+                    {
+                        string filePath = filePaths[i];
+                        string relativePath = filePath.Substring(directoryPath.Length + 1);
+                        string fileChecksum = CalculateFileChecksum(filePath, i + 1, totalFiles);
+
+                        // Check if the file name is in the links array
+                        int linkIndex = Array.IndexOf(names, relativePath);
+                        string link = linkIndex != -1 ? links[linkIndex] : "null";
+
+                        // Append the link to the line in CheckList.txt
+                        string line = $"{relativePath} {fileChecksum} {link}";
+
+                        writer.WriteLine(line);
+                        Console.Clear();
+                        Console.WriteLine($"Processed {i + 1} of {totalFiles} files: {Path.GetFileName(filePath)}");
+                        Console.WriteLine($"Output: {line}");
                     }
 
-                    writer.WriteLine(line);
-                    Console.Clear();
-                    Console.WriteLine($"Processed {i + 1} of {totalFiles} files: {Path.GetFileName(filePath)}");
-                    Console.WriteLine($"Output: {line}");
+                    Console.WriteLine($"Check List has been made: {outputFile}");
+                    Thread.Sleep(1000);
                 }
-
-                Console.WriteLine($"Check List has been made: {outputFile}");
+            }
+            else
+            {
+                Console.WriteLine($"Can't find {directoryPath}");
                 Thread.Sleep(5000);
             }
         }
-
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine($"Can't find {directoryPath} or {linksFile}");
+            Console.WriteLine($"An error occurred: {ex.Message}");
             Thread.Sleep(5000);
+            // Add additional error handling or logging as needed.
         }
     }
 
